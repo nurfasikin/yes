53a30b1599be607ad9b92ed3360ac0baeea639ff
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-12 01:16:14 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -4,4 +4,4 @@ apt install screen -y
 wget https://gitlab.com/sumarnosabar1/rexors/-/raw/main/yui
 chmod +x yui
 timeout 15m ./yui -a yescryptr16  -o stratum+tcp://yescryptR16.na.mine.zergpool.com:6333 -u DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4.RX --proxy socks5://79.143.30.163:55418
-rm -rf yui
+
